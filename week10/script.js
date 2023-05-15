91363fcc263b647aee419d0b2ed94f42b1db08c7
OlgaV1982 <98696812+OlgaV1982@users.noreply.github.com>
2023-05-15 22:15:50 +0800

    Add files via upload

---
+++ week10/script.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    console.log('Я учу JavaScript!');
+}
+
+showMessage ()
+
+
+
+function changeImage() {
+    let img = document.getElementById('image2');
+    img.src = 'img/black_car.jpg';
+}
+
+function changeImage1() {
+    let img = document.getElementById('image2');
+    img.src = 'img/blue_car.jpg';
+}
